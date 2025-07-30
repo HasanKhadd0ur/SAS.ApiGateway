a713e9f349f02d9a130b47423847b345783046d9
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-30 21:10:33 +0300

    Update PRogeram.CS

--- src/SAS.APIGateway.GatewayService/Program.cs
+++ src/SAS.APIGateway.GatewayService/Program.cs
@@ -1,5 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDevClient",
+        builder => builder
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials() 
+    );
+});
+
 // Load Reverse Proxy from appsettings.json
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
@@ -10,6 +21,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularDevClient");
 
 // Enable routing through proxy
 app.MapReverseProxy();
